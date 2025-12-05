3508a3f2c4a59d7169199f166869b04ee1cbf8c0
Benedikt Jutz <benedikt.jutz@kit.edu>
2025-12-05 17:18:58 +0100

    Collect Transaction Size, Models per Transaction

--- vsum/src/main/java/tools/vitruv/methodologisttemplate/vsum/VitruvChangeTimeObserver.java
+++ vsum/src/main/java/tools/vitruv/methodologisttemplate/vsum/VitruvChangeTimeObserver.java
@@ -2,7 +2,13 @@ package tools.vitruv.methodologisttemplate.vsum;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+
+import edu.kit.ipd.sdq.commons.util.java.Quadruple;
+import edu.kit.ipd.sdq.commons.util.java.Quintuple;
 import edu.kit.ipd.sdq.commons.util.java.Triple;
 import tools.vitruv.change.atomic.EChange;
 import tools.vitruv.change.atomic.uuid.Uuid;
@@ -10,27 +16,42 @@ import tools.vitruv.change.composite.description.PropagatedChange;
 import tools.vitruv.change.composite.description.VitruviusChange;
 import tools.vitruv.change.composite.propagation.ChangePropagationListener;
 
-public class VitruvChangeTimeObserver extends TimeObserver<Triple<Long, Long, Long>> implements ChangePropagationListener {
+public class VitruvChangeTimeObserver extends TimeObserver<Quintuple<Long, Long, Long, Integer, Integer>> implements ChangePropagationListener {
   private long vitruviusChangeCounter = 0;
   private long eChangeCounter = 0;
 
   public VitruvChangeTimeObserver() {
-    super(new String[]{"VitruviusChangeCounter", "NoOfEChanges", "Time"});
+    super(new String[]{"VitruviusChangeCounter", "NoOfEChanges", "Time", "NoOfAffectedModels", "NoOfAffectedElements"});
   }
 
   @Override
   public void finishedChangePropagation(Iterable<PropagatedChange> propagatedChanges) {
     var time = stopTiming();
     Set<EChange<?>> allEChanges = new HashSet<>();
+    Set<EObject> affectedModelElements = new HashSet<>();
+    Set<URI> affectedModelURIS = new HashSet<>();
     long eChangesRequired = eChangeCounter;
 
     for (var change : propagatedChanges) {
+      Stream.concat(
+        change.getOriginalChange().getAffectedEObjects().stream(),
+        change.getConsequentialChanges().getAffectedEObjects().stream())
+        .forEach(eObject -> affectedModelElements.add(eObject));
+      
+      Stream.concat(
+        change.getOriginalChange().getChangedURIs().stream(),
+        change.getConsequentialChanges().getChangedURIs().stream())
+        .forEach(uri -> affectedModelURIS.add(uri));
+
       allEChanges.addAll(change.getOriginalChange().getEChanges());
       allEChanges.addAll(change.getConsequentialChanges().getEChanges());
     };
     eChangesRequired += allEChanges.size();
 
-    timesPerChangeType.add(new Triple<Long,Long,Long>(vitruviusChangeCounter, eChangesRequired, time));
+    timesPerChangeType.add(new Quintuple<Long, Long, Long, Integer, Integer>(
+      vitruviusChangeCounter, eChangesRequired, time,
+      affectedModelURIS.size(),
+      affectedModelElements.size()));
     vitruviusChangeCounter += 1;
   }
 
@@ -41,7 +62,12 @@ public class VitruvChangeTimeObserver extends TimeObserver<Triple<Long, Long, Lo
   }
 
   @Override
-  protected String[] getLineFor(Triple<Long, Long, Long> record) {
-    return new String[] {record.getFirst().toString(), record.getSecond().toString(), record.getThird().toString()};
+  protected String[] getLineFor(Quintuple<Long, Long, Long, Integer, Integer> record) {
+    return new String[] {
+      record.get0().toString(), 
+      record.get1().toString(),
+      record.get2().toString(),
+      record.get3().toString(), 
+      record.get4().toString()};
   }
 }
